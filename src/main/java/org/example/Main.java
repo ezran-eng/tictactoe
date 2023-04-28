f5e000676a195aaeeb8be9a4b4405d8b7166233d
Ezequiel <eezquieloelero@gmail.com>
2023-04-28 10:20:49 -0300

    Finished 1/1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
-//Primero comentario
+/*
+
+Descripción
+El tres en raya es un juego conocido en todo el mundo. Cada país puede tener su propia versión del nombre, a veces las reglas son diferentes, pero la esencia del juego sigue siendo la misma. A continuación se presentan las reglas principales que pueden resultarle familiares desde la infancia.
+El tres en raya se juega por dos jugadores en una cuadrícula de 3x3. Uno de los jugadores es 'X', y el otro jugador es 'O'. X juega primero, luego O toma el siguiente turno, y así sucesivamente.
+Los jugadores escriben 'X' y 'O' en un campo de 3x3.
+El primer jugador que coloca 3 X o 3 O en línea recta (incluyendo diagonales) gana el juego.
+
+Objetivos
+Tu primera tarea en este proyecto es imprimir la cuadrícula del juego en la salida de la consola. Usa lo que has aprendido sobre la función print() para imprimir tres líneas, cada una conteniendo tres caracteres (X's y O's) para representar un juego de tres en raya en el que todos los campos de la cuadrícula han sido llenados.
+*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("X O X\n O X O\n X X O");
     }
 }
